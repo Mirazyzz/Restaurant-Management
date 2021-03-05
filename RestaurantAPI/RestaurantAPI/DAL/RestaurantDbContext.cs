cdf4229974e5dd1e2a04e00c50896db3ec947f1a
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2021-03-05 19:55:01 +0100

    add db context

---
+++ RestaurantAPI/RestaurantAPI/DAL/RestaurantDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.DAL
+{
+    public class RestaurantDbContext
+    {
+    }
+}
