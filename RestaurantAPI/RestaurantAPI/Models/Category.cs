058c0ebbd6f040b999b3a01088c316f3e23ce492
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2021-03-05 19:59:03 +0100

    added item & category models

---
+++ RestaurantAPI/RestaurantAPI/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace RestaurantAPI.Models
+{
+    public class Category
+    {
+        public int IdCategory { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ RestaurantAPI/RestaurantAPI/Models/Item.cs
@@ -0,0 +1,13 @@
+﻿namespace RestaurantAPI.Models
+{
+    public class Item
+    {
+        public int IdItem { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+    }
+}
